add recall,  precision & fix bugs in nep(), nde()

nilmtk/losses.py
@@ -1,5 +1,9 @@
+from numpy.lib.arraysetops import isin
+from numpy.lib.function_base import average
 from sklearn.metrics import mean_squared_error, mean_absolute_error, f1_score, r2_score
+from sklearn.metrics import precision_score, recall_score
 import numpy as np
+import pandas as pd
 
 def mae(app_gt,app_pred):
     return mean_absolute_error(app_gt,app_pred)
@@ -15,6 +19,33 @@ def f1score(app_gt, app_pred):
     pred_temp = np.where(pred_temp<threshold,0,1)
 
     return f1_score(gt_temp, pred_temp)
+
+def f1score_(app_gt, app_pred):
+    threshold = 10
+    gt_temp = np.array(app_gt)
+    gt_temp = np.where(gt_temp<threshold,0,1)
+    pred_temp = np.array(app_pred)
+    pred_temp = np.where(pred_temp<threshold,0,1)
+
+    return f1_score(gt_temp, pred_temp, average='macro')
+
+def recall(app_gt, app_pred):
+    threshold = 10
+    gt_temp = np.array(app_gt)
+    gt_temp = np.where(gt_temp<threshold,0,1)
+    pred_temp = np.array(app_pred)
+    pred_temp = np.where(pred_temp<threshold,0,1)
+
+    return recall_score(gt_temp, pred_temp, )
+
+def precision(app_gt, app_pred):
+    threshold = 10
+    gt_temp = np.array(app_gt)
+    gt_temp = np.where(gt_temp<threshold,0,1)
+    pred_temp = np.array(app_pred)
+    pred_temp = np.where(pred_temp<threshold,0,1)
+
+    return precision_score(gt_temp, pred_temp, )
 
 def relative_error(app_gt,app_pred):
     constant = 1
@@ -32,7 +63,14 @@ def nde(app_gt,app_pred):
     numerator = np.sum((app_gt-app_pred)**2)
     denominator = np.sum(app_gt**2)
 
-    return np.sqrt(numerator/denominator)
+    # if denominator < 1e-5:
+    #     denominator = 1e-5
+
+    frac = numerator/denominator
+    if isinstance(frac, pd.Series):
+        return np.sqrt(frac[0])
+    else:
+        return np.sqrt(frac)
 
 def nep(app_gt,app_pred):
     # Normalized Error in Assigned Power (NEP)
@@ -40,4 +78,11 @@ def nep(app_gt,app_pred):
     numerator = np.sum(np.abs(app_gt-app_pred))
     denominator = np.sum(app_gt)
 
-    return numerator/denominator
+    # if denominator < 1e-5:
+    #     denominator = 1e-5
+
+    frac = numerator/denominator
+    if isinstance(frac, pd.Series):
+        return np.sqrt(frac[0])
+    else:
+        return np.sqrt(frac)